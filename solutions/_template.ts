5bc3cf50ea713f6589603a939a27e848e7b511b5
CactusPuppy <puppycactus@gmail.com>
2025-12-02 22:46:15 -0800

    chore: add block to template for code that should only run when solving

--- solutions/_template.ts
+++ solutions/_template.ts
@@ -1,3 +1,7 @@
 import { readFileSync } from "node:fs";
 
 const input = readFileSync("./inputs/day01-input.txt").toString();
+
+if (import.meta.main) {
+  console.log("Problem solving code here!");
+}
